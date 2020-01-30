d670bf88df62ad5c23ae670353dda48818ee18ea
Manato Inoue <tarou.xxx.0129@gmail.com>
2020-01-31 05:11:40 +0900

    Update blogs test.py

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,16 +1,16 @@
 """ Main app(blogs) tests """
 import io
 import datetime
+import urllib.parse
 from PIL import Image
-from django.core.files import File
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 
-from .models import Post
+from .models import Post, Comment, PostHit, PostEval
 
 
 User = get_user_model()
@@ -31,7 +31,7 @@ def create_post(author, title, days, text='TEST'):
     Create post object 
     
     Args:
-        author, title, days, text(default=TEST)
+        author, title, days(negative for past, positive for future), text(default=TEST)
     """
     img = make_dummy_image()
     time = timezone.now() + datetime.timedelta(days=days)
@@ -46,9 +46,9 @@ def create_post(author, title, days, text='TEST'):
 
 
 class PostListViewTests(TestCase):
-    """ Test PostListView """
+    """ Tests PostListView """
     def setUp(self):
-        self.user = User.objects.create_user('tester', 'a@gfmk.com', 'password')
+        self.user = User.objects.create_user('tester', 'test@gmail.com', 'password')
 
     def test_no_post(self):
         """ If there are no posts, an appropriate message is displayed """
@@ -93,13 +93,59 @@ class PostListViewTests(TestCase):
             ['<Post: Past post>']
         )
 
- 
-class PostDetailViewTests(TestCase):
-    """ Test PostDetailView """
+
+class SearchResultPostViewTests(TestCase):
+    """ Tests SearchResultPostView"""
     def setUp(self):
-        self.user = User.objects.create_user('tester', 'a@gfmk.com', 'password')
-        self.user.icon_image = File(make_dummy_image())
-        self.user.save()
+        self.user = User.objects.create_user('testuser', 'test@gmail.com', 'password')
+        self.other_user = User.objects.create_user('other', 'other@gmail.com', 'password')
+
+    def test_no_post(self):
+        """ If there are no posts, the user are redirected to index page """
+        response = self.client.get(reverse('blogs:result'))
+        self.assertRedirects(response, reverse('blogs:index'))
+
+    def test_no_search_word(self):
+        """ If there are no search words, the user are redirected to index page """
+        response = self.client.get(reverse('blogs:result') + '?q=')
+        self.assertRedirects(response, reverse('blogs:index'))
+    
+    def test_search_post_by_author(self):
+        """ When the user search by author name, the matched posts are displayed """
+        post1 = create_post(author=self.user, title='post1', days=-30)
+        post2 = create_post(author=self.user, title='post2', days=-60)
+        q = dict(q=self.user.username)
+        response = self.client.get(reverse('blogs:result')  + '?' + urllib.parse.urlencode(q))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['result_post_list'], 
+            ['<Post: post1>', '<Post: post2>']
+        )
+    
+    def test_search_post_by_title(self):
+        """ When the user search by title, the matched posts are displayed """
+        post1 = create_post(author=self.user, title='testpost', days=-30)
+        post2 = create_post(author=self.other_user, title='testpost', days=-20)
+        q = dict(q='testpost')
+        response = self.client.get(reverse('blogs:result')  + '?' + urllib.parse.urlencode(q))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['result_post_list'], 
+            ['<Post: testpost>', '<Post: testpost>']
+        )
+    
+    def test_not_matched_with_post(self):
+        post1 = create_post(author=self.user, title='testpost', days=-30)
+        q = dict(q='mismatch_word')
+        response = self.client.get(reverse('blogs:result') + '?' + urllib.parse.urlencode(q))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['result_post_list'], [])
+
+
+class PostDetailViewTests(TestCase):
+    """ Tests PostDetailView """
+    def setUp(self):
+        self.user = User.objects.create_user('tester', 'test@gmail.com', 'password')
 
     def test_past_post(self):
         """ Posts with a published_date in the past are displayed on detail page """
@@ -112,11 +158,27 @@ class PostDetailViewTests(TestCase):
         future_post = create_post(author=self.user, title='Future post', days=+30)
         response = self.client.get(reverse('blogs:detail', args=(future_post.id,)))
         self.assertEqual(response.status_code, 404)
+    
+    def test_get_comment(self):
+        """ Comments related with the post are displayed on detail page """
+        post = create_post(author=self.user, title='testpost', days=-30)
+        comment = Comment.objects.create(user=self.user, post=post, text='comment', created_date=timezone.now(), updated_date=timezone.now())
+        response = self.client.get(reverse('blogs:detail', args=(post.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['comment_list'], ['<Comment: Comment object (1)>'])
+
+    def test_get_hit(self):
+        """ Post hits of the post are displayed on detail page """
+        post = create_post(author=self.user, title='testpost', days=-30)
+        hit = PostHit.objects.create(user=self.user, post=post, hit=True, created_date=timezone.now())
+        response = self.client.get(reverse('blogs:detail', args=(post.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['post_hit'], ['<PostHit: PostHit object (1)>'])
 
 
 class PostCreateViewTests(TestCase):
-    """ Test PostCreateView"""
-    def test_not_logined_post(self):
+    """ Tests PostCreateView"""
+    def test_not_logged_in_post(self):
         """ If the user are not authenticated, they are redirected to login page """
         form_data = {
             'title': 'Post',
@@ -124,13 +186,12 @@ class PostCreateViewTests(TestCase):
             'image': make_dummy_image()
         }
         response = self.client.post(reverse('blogs:post'), data=form_data)
-        query = dict(next=reverse('blogs:post'))
         self.assertRedirects(
             response,
-            reverse('login') + ''.join('?{}={}'.format(key, value) for key, value in query.items())
+            reverse('login') + '?next=' + reverse('blogs:post')
         )
 
-    def test_logined_post(self):
+    def test_logged_in_post(self):
         """ If the user are authenticated, they can create post and are redirected to index page """
         self.client.force_login(
             User.objects.create_user('tester', 'test@gmail.com', 'password'))
@@ -144,13 +205,13 @@ class PostCreateViewTests(TestCase):
 
 
 class PostUpdateViewTests(TestCase):
-    """ Test PostUpdateView """
+    """ Tests PostUpdateView """
     def setUp(self):
         self.user = User.objects.create_user('tester', 'test@gmail.com', 'password')
         self.post = create_post(author=self.user, title='Post', days=-30)
         self.post.save()
 
-    def test_not_logined_update(self):
+    def test_not_logged_in_update(self):
         """ If the user are not authenticated, they are redirected to login page """
         change_data = {
             'title': 'Changed Post',
@@ -158,10 +219,9 @@ class PostUpdateViewTests(TestCase):
             'image': make_dummy_image()
         }
         response = self.client.put(reverse('blogs:edit', args=(self.post.pk,)), data=change_data)
-        query = dict(next=reverse('blogs:edit', args=(self.post.pk,)))
         self.assertRedirects(
             response,
-            reverse('login') + ''.join('?{}={}'.format(key, value) for key, value in query.items())
+            reverse('login') + '?next=' + reverse('blogs:edit', args=(self.post.pk,))
         )
 
     def test_other_user_update(self):
@@ -188,24 +248,23 @@ class PostUpdateViewTests(TestCase):
 
 
 class PostDeleteViewTests(TestCase):
-    """ Test PostDelateView """
+    """ Tests PostDelateView """
     def setUp(self):
-        self.user = User.objects.create_user('tester', 'a@gfmk.com', 'password')
+        self.user = User.objects.create_user('tester', 'test@gmail.com', 'password')
         self.post = create_post(author=self.user, title='Post', days=-30)
         self.post.save()
 
-    def test_not_logined_delete(self):
+    def test_not_logged_in_delete(self):
         """ If the user are not authenticated, they are redirected to login page """
         response = self.client.delete(reverse('blogs:delete', args=(self.post.pk,)))
-        query = dict(next=reverse('blogs:delete', args=(self.post.pk,)))
         self.assertRedirects(
             response,
-            reverse('login') + ''.join('?{}={}'.format(key, value) for key, value in query.items())
+            reverse('login') + '?next=' + reverse('blogs:delete', args=(self.post.pk,))
         )
 
     def test_other_user_delete(self):
         """ If other user try to delete the post, it returns 403 """
-        self.client.force_login(User.objects.create_user('other_user', 'b@gfmk.com', 'password'))
+        self.client.force_login(User.objects.create_user('other_user', 'other@gmail.com', 'password'))
         response = self.client.delete(reverse('blogs:delete', args=(self.post.pk,)))
         self.assertEqual(response.status_code, 403)
 
@@ -214,3 +273,99 @@ class PostDeleteViewTests(TestCase):
         self.client.force_login(self.user)
         response = self.client.delete(reverse('blogs:delete', args=(self.post.pk,)))
         self.assertRedirects(response, reverse('blogs:index'))
+
+
+class UserPageViewTests(TestCase):
+    """ Tests UserPageView """
+    def setUp(self):
+        self.user = User.objects.create_user('tester', 'test@gmail.com', 'password')
+
+    def test_not_logged_in(self):
+        """ Even if the user are not authenticated, user page is displayed """
+        dest_user = self.user
+        response = self.client.get(reverse('blogs:user_page', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_visit_by_myself(self):
+        """ If the user visit their own user page, it is displayed """
+        dest_user = self.user
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('blogs:user_page', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_visit_by_other(self):
+        """ Even if the user visit other user page, it is displayed """
+        dest_user = User.objects.create_user('other', 'other@gmail.com', 'password')
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('blogs:user_page', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
+
+
+class UserPostListViewTests(TestCase):
+    """ Tests UserPostListView """
+    def setUp(self):
+        self.user = User.objects.create_user('tester', 'test@gmail.com', 'password')
+
+    def test_not_logged_in(self):
+        """ Even if the user are not authenticated, user posts page is displayed """
+        dest_user = self.user
+        response = self.client.get(reverse('blogs:user_posts', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_visit_by_myself(self):
+        """ If the user visit their own user posts page, it is displayed """
+        dest_user = self.user
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('blogs:user_posts', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_visit_by_other(self):
+        """ Even if the user visit other user posts page, it is displayed """
+        dest_user = User.objects.create_user('other', 'other@gmail.com', 'password')
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('blogs:user_posts', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_past_post(self):
+        """ Posts with a published_date in the past are displayed on user posts page """
+        dest_user = self.user
+        past_post = create_post(author=self.user, title='Past post', days=-30)
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('blogs:user_posts', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['my_post_list'], ['<Post: Past post>'])
+
+    def test_future_post(self):
+        """ Posts with a published_date in the future are not displayed on user posts page """
+        dest_user = self.user
+        future_post = create_post(author=self.user, title='Future post', days=+30)
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('blogs:user_posts', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['my_post_list'], [])
+
+
+class UserProfileViewTests(TestCase):
+    """ Tests UserProfileView """
+    def setUp(self):
+        self.user = User.objects.create_user('tester', 'test@gmail.com', 'password')
+
+    def test_not_logged_in(self):
+        """ Even if the user are not authenticated, user profile page is displayed """
+        dest_user = self.user
+        response = self.client.get(reverse('blogs:user_profile', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_visit_by_myself(self):
+        """ If the user visit their own user profile page, it is displayed """
+        dest_user = self.user
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('blogs:user_profile', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_visit_by_other(self):
+        """ Even if the user visit other user profile page, it is displayed """
+        dest_user = User.objects.create_user('other', 'other@gmail.com', 'password')
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('blogs:user_profile', args=(dest_user.pk,)))
+        self.assertEqual(response.status_code, 200)
